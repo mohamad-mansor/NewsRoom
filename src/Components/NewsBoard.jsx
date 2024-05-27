6c1d1273ac47c9726db4b6e0c8a35120236817e9
Mohamad <mohamad.alhajr@dci-student.org>
2024-05-27 10:06:17 +0200

    README edited

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,19 +5,19 @@ import "./NewsBoard.css";
 function NewsBoard({ category, searchTerm }) {
   const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    if (searchTerm) {
-      url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${
-        import.meta.env.VITE_API_KEY
-      }`;
-    }
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setArticles(data.articles));
-  }, [category, searchTerm]);
+  // useEffect(() => {
+  //   let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+  //     import.meta.env.VITE_API_KEY
+  //   }`;
+  //   if (searchTerm) {
+  //     url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${
+  //       import.meta.env.VITE_API_KEY
+  //     }`;
+  //   }
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => setArticles(data.articles));
+  // }, [category, searchTerm]);
 
   return (
     <div className="news-board container">
